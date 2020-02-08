7d3dd4f0ae5f554981cbb40b046b3a7ccdb6e820
Henning <henning.flath@t-online.de>
2020-02-08 20:18:51 +0100

    Added Entities, SCALE and more

---
+++ src/com/jnh/game/gameObjects/entities/Entity.java
@@ -0,0 +1,67 @@
+package com.jnh.game.gameObjects.entities;
+
+import com.jnh.game.gameObjects.GameObject;
+import com.jnh.game.state.GameState;
+import com.jnh.game.utils.assets.Sprite;
+
+/**
+ * Dies ist die Klasse zur Verwaltung aller lebender und sich bewegender Lebewesen. Dazu gehören Menschen, der Player, Monster, Tiere usw., aber keine Pflanzen, die sich ja nicht bewegen können und kein wirkliches Health-System benötigen.
+ * 
+ * @author Henning
+ *
+ */
+public abstract class Entity extends GameObject {
+	
+	private int maxHealth;
+	private int health;
+	private float speed;
+	
+	public Entity(GameState state, Sprite sprite, float x, float y, float width, float height) {
+		super(state, sprite, x, y, width, height);
+	}
+
+	/**
+	 * @return die maximale Anzahl an Gesundheit, die die Entity erhalten kann. Theoretisch kann das umgangen werden, aber dies sollte nur bei Dingen wie Magie geschehen, aber natürliche Regeneration sollte das nicht erlauben.
+	 */
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+	/**
+	 * @param maxHealth die maximale Anzahl an Gesundheit, die die Entity erhalten kann. Theoretisch kann das umgangen werden, aber dies sollte nur bei Dingen wie Magie geschehen, aber natürliche Regeneration sollte das nicht erlauben.
+	 */
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+
+	/**
+	 * @return der momentante Betrag an Gesundheit
+	 */
+	public int getHealth() {
+		return health;
+	}
+
+	/**
+	 * @param health der momentane Betrag an Gesundheit
+	 */
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	/**
+	 * @return die Geschwindigkeit
+	 */
+	public float getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * @param die Geschwindigkeit
+	 */
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+	
+	
+	
+}
---
+++ src/com/jnh/game/utils/Global.java
@@ -0,0 +1,16 @@
+package com.jnh.game.utils;
+
+/**
+ * 
+ * Verschiedene globale Konstanten.
+ * 
+ * @author Henning
+ *
+ */
+public class Global {
+	
+	/**
+	 * Wie viele Pixel einer Einheit im Spiel entsprechen.
+	 */
+	public static final int SCALE = 16;
+}
